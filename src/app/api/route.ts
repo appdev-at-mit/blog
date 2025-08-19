ea830c592ae95e2b13ade99b81517862bed829a5
Jieruei Chang <knosmos@gmail.com>
2025-08-19 20:00:30 +0800

    let's get to the cors of this

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
     },
   });
 }
